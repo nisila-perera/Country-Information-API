bf5c7edc4ce26ca4b6d41b8afc7e3fd4cd5a8169
Nisila Perera <nisilainsitha1@gmail.com>
2024-07-03 13:29:30 +0530

    last

--- app.js
+++ app.js
@@ -1,51 +1,44 @@
 fetch("https://restcountries.com/v3.1/all")
-.then(res=>res.json())
-.then(data=>{
-    let tblCountries=document.getElementById("tbl");
+  .then((res) => res.json())
+  .then((data) => {
+    let tblCountries = document.getElementById("tbl");
 
-    let tblBody=`<tr>
+    let tblBody = `<tr>
                     <th>Name</th>
+                    <th>Capital</th>
                     <th>Flag</th>
                 </tr>`;
 
-    data.forEach(element => {
-        tblBody+=`<tr>
+    data.forEach((element) => {
+      tblBody += `<tr>
                     <td>${element.name.common}</td>
-                    <td>${element.flag}</td>
-                </tr>`
+                    <td>${element.capital}</td>
+                    <td><img src="${element.flags.png}" width="50px"></td>
+                </tr>`;
     });
 
-    tblCountries.innerHTML=tblBody;
-})
+    tblCountries.innerHTML = tblBody;
+  });
 
+function serch() {
+  let searchValue = document.getElementById("txtSearchValue").value;
 
+  let officialName = document.getElementById("officialName");
+  let name = document.getElementById("name");
+  let capital = document.getElementById("capital");
+  let img = document.getElementById("img");
 
-// https://restcountries.com/v3.1/name/{name}
+  console.log(searchValue);
+  fetch(`https://restcountries.com/v3.1/name/${searchValue}`)
+    .then((res) => res.json())
+    .then((data) => {
+      data.forEach((obj) => {
+        officialName.innerText = obj.name.official;
+        name.innerText = obj.name.common;
+        capital.innerText = obj.capital[0];
 
-
-function serchCuntrie(){
-    let searchValue=document.getElementById("txtSearchValue").value;
-
-    let officialName = document.getElementById("officialName");
-   let name = document.getElementById("name")
-   let img =  document.getElementById("img")
-
-
-    console.log(searchValue);
-    fetch(`https://restcountries.com/v3.1/name/${searchValue}`)
-    .then(res=>res.json())
-    .then(data =>{
-
-        data.forEach(obj=>{
-            officialName.innerText = obj.name.official;
-            name.innerText = obj.name.common;
-     
-            img.innerHTML=`<img src="${obj.flags.png}" alt="">`
-        })
-        console.log(data);
-
-
-
-    
-    })
+        img.innerHTML = `<img src="${obj.flags.png}" alt="">`;
+      });
+      console.log(data);
+    });
 }
